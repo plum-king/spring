d64d8a7d5a4d7a03f74036e764b6be012ec4dddc
plum-king <jeesoo0908@gmail.com>
2024-06-08 20:43:12 +0900

    [Week 8] 예외 처리

---
+++ src/main/java/umc/study/apiPayload/code/TempResponseDTO.java
@@ -0,0 +1,24 @@
+package umc.study.apiPayload.code;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class TempResponseDTO {
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TempTestDTO{
+        String testString;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class TempExceptionDTO{
+        Integer flag;
+    }
+}
